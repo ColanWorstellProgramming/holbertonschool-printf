bdbef0216aea550a9357229ec16b184c200482ec
Colan Worstell <Cworstellprogramming@gmail.com>
2022-11-01 14:49:52 -0700

    addingMainFileAfterTests

--- main.h
+++ main.h
@@ -2,6 +2,25 @@
 #define h
 #undef h
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+#include <stddef.h>
+
+
 int _printf(const char *format, ...);
+int _putchar(char c);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_di(va_list di);
+
+typedef struct printformat
+{
+char *theC;
+int (*z)(va_list);
+} form;
 
 #endif
